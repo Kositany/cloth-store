5e233d49d1ca5f9a2eb4e1f422a884df45d8eb4e
Kositany <kipkosi3@gmail.com>
2025-09-02 22:36:32 +0300

    Add component and context index files for better module resolution

---
+++ src/components/index.ts
@@ -0,0 +1,4 @@
+export { default as Navigation } from './Navigation';
+export { default as Footer } from './Footer';
+export { default as Hero } from './Hero';
+export { default as FeaturedCollections } from './FeaturedCollections';
